659afe0b3bec24649a0c1407b01e0ba7129b88a2
Wan Lucas <wan.lucas.90@gmail.com>
2023-03-12 17:18:51 -0300

    feat(entities): Goal

---
+++ server/src/domain/entities/Goal.ts
@@ -0,0 +1,71 @@
+export interface IGoal {
+  description: string;
+  id: string;
+  branchId: string;
+  finalDate: string;
+  difficulty: number;
+}
+
+export default class Goal {
+  public readonly description: string;
+  public readonly id: string;
+  public readonly branchId: string;
+  public readonly finalDate: string;
+  public readonly difficulty: number; 
+
+  constructor ({ description, id, branchId, finalDate, difficulty }: IGoal) {
+    if (!description) {
+      throw new Error('Goal must have a description');
+    }
+
+    if (typeof description !== 'string') {
+      throw new Error('Goal description must be a string');
+    }
+
+    if (!id) {
+      throw new Error('Goal must have an id');
+    }
+
+    if (typeof id !== 'string') {
+      throw new Error('Goal id must be a string');
+    }
+
+    if (!branchId) {
+      throw new Error('Goal must have a branch id');
+    }
+
+    if (typeof branchId !== 'string') {
+      throw new Error('Goal branch id must be a string');
+    }
+
+    if (!finalDate) {
+      throw new Error('Goal must have a final date');
+    }
+
+    if (typeof finalDate !== 'string') {
+      throw new Error('Goal final date must be a string');
+    }
+
+    if (new Date(finalDate).getTime() < new Date().getTime()) {
+      throw new Error('Goal final date must be in the future');
+    }
+
+    if (!difficulty) {
+      throw new Error('Goal must have a difficulty');
+    }
+
+    if (typeof difficulty !== 'number') {
+      throw new Error('Goal difficulty must be a number');
+    }
+
+    if (difficulty < 1 || difficulty > 10) {
+      throw new Error('Goal difficulty must be between 1 and 10');
+    }
+
+    this.description = description;
+    this.id = id;
+    this.branchId = branchId;
+    this.finalDate = finalDate;
+    this.difficulty = difficulty;
+  }
+}
