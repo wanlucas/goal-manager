397873515928a43cb056590f8691b72258904e3e
Wan Lucas <wan.lucas.90@gmail.com>
2023-03-19 17:13:51 -0300

    test(useCases): getUser

--- server/test/unit/useCases/user.test.ts
+++ server/test/unit/useCases/user.test.ts
@@ -1,6 +1,9 @@
 import User from '../../../src/domain/entities/User';
 import UserRepository from '../../../src/domain/repositories/UserRepository';
 import CreateUser, { CreateUserDTO } from '../../../src/domain/useCases/user/CreateUser';
+import GetUser from '../../../src/domain/useCases/user/GetUser';
+import GetUsers from '../../../src/domain/useCases/user/GetUsers';
+import usersMock from '../../mocks/user';
 
 describe('User use cases', () => {
   describe('CreateUser', () => {
@@ -20,6 +23,32 @@ describe('User use cases', () => {
       const result = await createUser.execute(payload);
   
       expect(result).toBeInstanceOf(User);
+    });
+  });
+
+  describe('GetUsers', () => {
+    const UserRepository: UserRepository = {
+      findAll: () => Promise.resolve(usersMock),
+    } as unknown as UserRepository;
+
+    test('should return a list of Users', async () => {
+      const getUsers = new GetUsers(UserRepository);
+      const result = await getUsers.execute();
+  
+      expect(result).toEqual(usersMock);
+    });
+  });
+
+  describe('GetUser', () => {
+    const UserRepository: UserRepository = {
+      findById: () => Promise.resolve(usersMock[0]),
+    } as unknown as UserRepository;
+
+    test('should find a User', async () => {
+      const getUser = new GetUser(UserRepository);
+      const result = await getUser.execute(usersMock[0].id);
+  
+      expect(result).toEqual(usersMock[0]);
     });
   });
 });
