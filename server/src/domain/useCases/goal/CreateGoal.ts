436688b018aa9b47859e8f1b9966de3b8c2627e3
Wan Lucas <wan.lucas.90@gmail.com>
2023-03-19 17:57:21 -0300

    feat(useCases): validation of branch existence when creating goal

--- server/src/domain/useCases/goal/CreateGoal.ts
+++ server/src/domain/useCases/goal/CreateGoal.ts
@@ -1,26 +1,35 @@
 import Goal from '../../entities/Goal';
 import GoalRepository from '../../repositories/GoalRepository';
 import { v4 as uuid } from 'uuid';
+import BranchRepository from '../../repositories/BranchRepository';
 
 export interface CreateGoalDTO {
-    description: string;
-    branchId: string;
-    finalDate: string;
-    difficulty: number;
-    target?: number;
-    isCompleted?: boolean;
+  description: string;
+  branchId: string;
+  finalDate: string;
+  difficulty: number;
+  target?: number;
+  isCompleted?: boolean;
 }
 
 export default class CreateGoal {
   private goalRepository: GoalRepository;
+  private branchRepository: BranchRepository;
   
-  constructor(goalRepository: GoalRepository) {
+  constructor(goalRepository: GoalRepository, branchRepository: BranchRepository) {
     this.goalRepository = goalRepository;
+    this.branchRepository = branchRepository;
   }
 
   async execute(payload: CreateGoalDTO): Promise<Goal> {
     const id = uuid();
     const goal = new Goal({ ...payload, id });
+
+    const foundBranch = await this.branchRepository.findById(goal.branchId);
+
+    if (!foundBranch) {
+      throw new Error('Branch not found');
+    }
 
     await this.goalRepository.save(goal);
 
