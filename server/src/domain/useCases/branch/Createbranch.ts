e97c4d8750a126abd99205f551ed1f60ac0396c5
Wan Lucas <wan.lucas.90@gmail.com>
2023-03-19 17:48:31 -0300

    feat(useCases): user existence validation when creating branch

--- server/src/domain/useCases/branch/Createbranch.ts
+++ server/src/domain/useCases/branch/Createbranch.ts
@@ -1,6 +1,7 @@
 import Branch from '../../entities/Branch';
 import BranchRepository from '../../repositories/BranchRepository';
 import { v4 as uuid } from 'uuid';
+import UserRepository from '../../repositories/UserRepository';
 
 export interface CreateBranchDTO {
   name: string;
@@ -11,14 +12,22 @@ export interface CreateBranchDTO {
 
 export default class CreateBranch {
   private branchRepository: BranchRepository;
+  private userRepository: UserRepository;
 
-  constructor(branchRepository: BranchRepository) {
+  constructor(branchRepository: BranchRepository, userRepository: UserRepository) {
     this.branchRepository = branchRepository;
+    this.userRepository = userRepository;
   }
 
   async execute(payload: CreateBranchDTO): Promise<Branch> {
     const id = uuid();
     const branch = new Branch({ ...payload, id });
+
+    const foundUser = await this.userRepository.findById(branch.userId);
+
+    if (!foundUser) {
+      throw new Error('User not found');
+    }
 
     await this.branchRepository.save(branch);
 
